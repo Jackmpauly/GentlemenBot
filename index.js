b6ee123e84083fa89a44c0ffafc487b31f1b9d19
jackmpauly <pauly@chapman.edu>
2021-03-18 17:46:47 -0700

    added example config and txt files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const config = require('./config.json')
+const config = require('./config.json') // On Github, this should be replaced by config2.json
 const client = new Discord.Client()
 
 const ms = require('ms')
