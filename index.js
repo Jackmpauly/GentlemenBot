9f60c9a05c3196942061548e2ad4df831edf4160
Suneel Freimuth <suneelfreimuth1@gmail.com>
2023-01-20 18:25:29 -0800

    Obviate config2.json by adding option to dump default config

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+const fs = require('fs')
+
+if (process.argv.includes('--dump-config')) {
+    console.log(`{
+    "token": "<TOKEN>",
+
+    "prefix": "$",
+    "prefixALT": "£",
+    
+    "adminRole": "<NAME OF ADMIN ROLE>",
+    "coAdminRole": "<NAME OF CO-ADMIN ROLE>",
+    "protectedRole": "<NAME OF PROTECTED ROLE>",
+    "botRole": "GentlemenBot",
+    "defaultRole": "<NAME OF BASE ROLE>",
+    "handicapRole": "<NAME OF HANDICAP ROLE>",
+
+    "autoReplyID": "<ID OF USER TO REPLY TO>",
+    "autoReplyContent": "Hi, user!",
+    
+    "customMiscCommand": "hello",
+    "customMiscCommandString": "Hello world! I'm ",
+    "defaultMiscCommandArg": "Gentlemen Bot",
+    
+    "defaultCooldownTime": "1h",
+    "handicapCooldownTime": "30m",
+    
+    "botActivity": "a game right now",
+    "botActivityType": "PLAYING",
+    
+    "memberIDsTxt": "SampleMemberIDs.txt",
+    "quotesTxt": "SampleCommands.txt",
+    
+    "defaultDMMessage": "help"
+}`)
+    process.exit(0)
+}
+
+try {
+    config = JSON.parse(fs.readFileSync('config.json'))
+} catch (err) {
+    console.error('Failed to read config.json:', err)
+    console.error('Get a default config by running with --dump-config:')
+    console.error('  $ node index.js --dump-config > config.json')
+    process.exit(1)
+}
+
 const Discord = require('discord.js')
-config = require('./config.json') // On Github, this should be replaced by config2.json
-client = new Discord.Client()
+const client = new Discord.Client()
 
 require('./global.js')
 
@@ -10,7 +55,6 @@ refresh.refreshQuotes() // read the quotes text file and store to global variabl
 
 
 client.on('ready', () =>{
-    THEGENTLEMEN_GUILD = client.guilds.cache.get(config.guild)
     client.user.setActivity(config.botActivity, { type: config.botActivityType })
     
     startTime = (new Date()).toLocaleString()
@@ -20,6 +64,14 @@ client.on('ready', () =>{
 
 })
 
+client.on('error', err => {
+    console.error(err)
+})
+
 const commandHandler = require('./commands')
 client.on('message', commandHandler);
 client.login(config.token);
+
+process.on('exit', () => {
+    console.log('Shutting down...')
+})
